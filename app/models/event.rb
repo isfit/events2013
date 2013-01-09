f67ceee207ec2b5d702d2864fddc383568edd571
Erik Bergersen <bergersen.erik@gmail.com>
2013-01-09 19:18:57 +0100

    event dates controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :event_type
   belongs_to :event_place
+  has_many :event_dates
   attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id
 
 
---
+++ test/unit/helpers/admin/event_dates_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::EventDatesHelperTest < ActionView::TestCase
+end
