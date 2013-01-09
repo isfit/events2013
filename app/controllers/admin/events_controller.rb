f04a9364452d8e27a9f2f34bf43adff306f2a0e0
Erik Bergersen <bergersen.erik@gmail.com>
2013-01-09 13:35:31 +0100

    event controllers

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,22 @@
+class Admin::EventsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Events::Application.routes.draw do
+  get "events/edit"
+
+  get "events/update"
+
+  get "events/create"
+
+  get "events/new"
+
+  get "events/destroy"
+
+  get "events/index"
+
+  get "events/show"
+
   resources :sessions
   namespace :admin do
     resources :events
