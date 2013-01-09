35921e0075a7b7f061cd1b6f42041a45fc56d1b3
Erik Bergersen <bergersen.erik@gmail.com>
2013-01-09 15:40:55 +0100

    destroy, weight_move and index for admin events

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,30 @@
 class Admin::EventsController < ApplicationController
   def index
+    @events = Event.where(:deleted => false).order("weight DESC")
+  end
+
+  def weight_move
+    current = Event.find(params[:id])
+    if params[:up].to_i == 1
+      if current != Event.order("weight DESC").first
+        switch = Event.where("weight > #{current.weight} AND deleted = 0").order("weight ASC").first
+        current_weight = current.weight
+        current.weight = switch.weight
+        switch.weight = current_weight
+        switch.save
+        current.save
+      end
+    else
+      if current != Event.order("weight DESC").last
+        switch = Event.where("weight < #{current.weight} AND deleted = 0").order("weight DESC").first
+        current_weight = current.weight
+        current.weight = switch.weight
+        switch.weight = current_weight
+        switch.save
+        current.save
+      end
+    end
+    redirect_to admin_events_path
   end
 
   def show
@@ -18,5 +43,9 @@ class Admin::EventsController < ApplicationController
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.deleted = 1
+    @event.save
+    redirect_to admin_events_path
   end
 end
