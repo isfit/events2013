3660429bb646044d1d22b37c2eb0ab577cf3fad7
Dag-Inge Aas <daginge@gmail.com>
2013-01-09 15:36:56 +0100

    Add image to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,10 @@
 class Event < ActiveRecord::Base
   belongs_to :event_type
   belongs_to :event_place
-  attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id
+
+  has_attached_file :image, :styles => { :cover => "1200x400>", :square => "500x500>" }
+
+  attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id, :image
 
 
 end
