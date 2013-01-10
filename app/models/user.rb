163fe84e14cb95baf98b70315057830fbf5cbb44
Erik Bergersen <bergersen.erik@gmail.com>
2013-01-10 16:21:02 +0100

    admin users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :positions
 
   has_secure_password
-
+  
   def workshop_leader?
     #groups.include?(Group.find(33))
     true
   end
 
   def admin?
-    # IT eller redaksjonen
-    groups.include?(Group.find(89)) || groups.include?(Group.find(99))
+    admin_users = [347, 78, 829, 471, 841, 882]
+    groups.include?(Group.find(89)) || admin_users.include?(id)
   end
 end
