61c1b2c45deecf6598233c64cd104652eac28b88
Erik Bergersen <bergersen.erik@gmail.com>
2013-01-11 16:56:09 +0100

    migration for image_front

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,9 @@ class Event < ActiveRecord::Base
   has_many :event_dates
 
   has_attached_file :image, :styles => { :cover => "1200x400>", :square => "500x500>" }
+  has_attached_file :image_front, :styles => { :cover => "1200x600>", :square => "500x500>" }
 
-  attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id, :image
+  attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id, :image, :image_front
 
 
 end
