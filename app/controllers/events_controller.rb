05d476b29b9c72ebdce0baf498d65e4e7035f180
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2013-01-11 17:51:58 +0100

    Index only shows published events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,21 @@
 class EventsController < ApplicationController
   def index
-
     evts = EventDate.includes(:event).order("datetime ASC")
     
     @events = []
     [7,8,9,10,11,12,13,14,15,16,17].each do |tall|
       ev = (evts.includes(:event).where('extract(day from datetime) = ?', tall))
-      @events << ev
+      
+      hack = []
+      if ev.length > 0
+        ev.each do |x|
+          if x.event.publish_at < Time.now
+            hack << x
+          end
+        end
+      end
+
+      @events << hack
     end
   end
 
