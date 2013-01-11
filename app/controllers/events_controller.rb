74b112c160c8a1d0a0ec2c48c6139f1a94fc6ad0
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2013-01-11 13:57:07 +0100

    Events fetched sorted from database

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,12 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+
+    evts = EventDate.includes(:event).order("datetime ASC")
+    
+    @events = []
+    [7,8,9,10,11,12,13,14,15,16,17].each do |tall|
+      @events << (evts.includes(:event).where('extract(day from datetime) = ?', tall))
+    end
   end
 
   def show
