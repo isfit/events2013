afdd7adb93495800df139ea94c08b28ef6ef4146
Dag-Inge Aas <daginge@gmail.com>
2013-01-13 22:34:13 +0100

    Added format helper

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.8'
 gem 'mysql2'
 gem 'simple_form'
 gem 'paperclip'
+gem 'bluecloth'
 
 
 # Gems used only for assets and not required
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	def format(str)
+	    bc = BlueCloth.new(str) 
+	    text = bc.to_html
+	    return text.html_safe
+	end
+
 end
