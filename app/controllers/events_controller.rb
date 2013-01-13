4b46e9f06b2fd4fa167cdc945ddacc2beeac4947
Erik Bergersen <bergersen.erik@gmail.com>
2013-01-13 17:58:10 +0100

    cors support for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,28 @@
 class EventsController < ApplicationController
+before_filter :cors_preflight_check
+after_filter :cors_set_access_control_headers
 
-  def index
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'GET'
+  headers['Access-Control-Max-Age'] = "1728000"
+end
+
+# If this is a preflight OPTIONS request, then short-circuit the
+# request, return only the necessary headers and return an empty
+# text/plain.
+
+def cors_preflight_check
+  if request.method == :options
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Max-Age'] = '1728000'
+    render :text => '', :content_type => 'text/plain'
+  end
+end
+
+def index
 
     
 
