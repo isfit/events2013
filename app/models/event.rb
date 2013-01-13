c54383916b8cd44d18e76850ef3b6d17a124f1d3
Erik Bergersen <bergersen.erik@gmail.com>
2013-01-13 15:48:40 +0100

    Fixed image size

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,8 @@ class Event < ActiveRecord::Base
   belongs_to :event_place
   has_many :event_dates
 
-  has_attached_file :image, :styles => { :cover => "1200x400>", :square => "500x500>" }
-  has_attached_file :image_front, :styles => { :cover => "1200x600>", :square => "500x500>" }
+  has_attached_file :image, :styles => { :cover => "920x420>", :square => "500x500>" }
+  has_attached_file :image_front, :styles => { :cover => "920x220>", :square => "500x500>" }
 
   attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id, :image, :image_front
 
