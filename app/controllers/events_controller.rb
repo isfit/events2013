f06b340d0907b92962f4ab43791e01e193b186b6
Dag-Inge Aas <daginge@gmail.com>
2013-01-22 20:45:43 +0100

    cleaned up indenting in eventscontroller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,59 +1,59 @@
 class EventsController < ApplicationController
-before_filter :cors_preflight_check
-after_filter :cors_set_access_control_headers
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
-def cors_set_access_control_headers
-  headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Methods'] = 'GET'
-  headers['Access-Control-Max-Age'] = "1728000"
-end
-
-# If this is a preflight OPTIONS request, then short-circuit the
-# request, return only the necessary headers and return an empty
-# text/plain.
-
-def cors_preflight_check
-  if request.method == :options
+  def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'GET'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-    headers['Access-Control-Max-Age'] = '1728000'
-    render :text => '', :content_type => 'text/plain'
-  end
-end
-
-def index
-
-  # Gets all eventdates and sorts them based on date and weight
-  @events = EventDate.joins(:event).order("start_at ASC, events.weight DESC")
-
-  if not (current_user && current_user.admin?)
-    @events = @events.where("publish_at < '#{Time.now}'")
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 
-  @festival_events = @events.where("all_festival = 1")
-  @events = @events.where("all_festival IS NULL OR all_festival = 0")
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
 
-  if params.has_key?(:eventtype)
-    @events = @events.joins(:event => :event_type).where('event_types.name = ?', params['eventtype'].tr('_', ' '))
-    @festival_events = @festival_events.joins(:event => :event_type).where('event_types.name = ?', params['eventtype'].tr('_', ' '))
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'GET'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+  def index
+
+    # Gets all eventdates and sorts them based on date and weight
+    @events = EventDate.joins(:event).order("start_at ASC, events.weight DESC")
+
+    if not (current_user && current_user.admin?)
+      @events = @events.where("publish_at < '#{Time.now}'")
+    end
+
+    @festival_events = @events.where("all_festival = 1")
+    @events = @events.where("all_festival IS NULL OR all_festival = 0")
+
+    if params.has_key?(:eventtype)
+      @events = @events.joins(:event => :event_type).where('event_types.name = ?', params['eventtype'].tr('_', ' '))
+      @festival_events = @festival_events.joins(:event => :event_type).where('event_types.name = ?', params['eventtype'].tr('_', ' '))
+
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @events.to_json( { :include =>  :event }) }
+    end
 
   end
-    
-  respond_to do |format|
-    format.html
-    format.json { render :json => @events.to_json( { :include =>  :event }) }
-  end
-  
-end
 
   def show
     @datetime = EventDate.find(params[:id])
     @event = @datetime.event
 
-      if not @event.publish_at <= Time.now || (current_user && current_user.admin?)
-        redirect_to root_url
-      end
+    if not @event.publish_at <= Time.now || (current_user && current_user.admin?)
+      redirect_to root_url
+    end
   end
 
   def api
@@ -70,12 +70,12 @@ end
       @events = @events.sort_by{|a| a.start_at}
     end
 
-      
+
     respond_to do |format|
       format.html
       format.json { render :json => @events.to_json( { :include =>  :event }) }
     end
-    
+
   end
 
   def iosapi
