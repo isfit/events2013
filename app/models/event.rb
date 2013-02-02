827b1605da217b283efe8b0625545240653ef839
Dag-Inge Aas <daginge@gmail.com>
2013-02-02 15:54:59 +0100

    Added compression to all images

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,8 @@ class Event < ActiveRecord::Base
   belongs_to :event_place
   has_many :event_dates
 
-  has_attached_file :image, :styles => { :cover => "920x420<", :square => "500x500>" }
-  has_attached_file :image_front, :styles => { :cover => "920x220<", :square => "500x500>" }
+  has_attached_file :image, :styles => { :cover => "920x420<", :square => "500x500>" }, :convert_options => { :all => "-quality 75" }
+  has_attached_file :image_front, :styles => { :cover => "920x220<", :square => "500x500>" }, :convert_options => { :all => "-quality 75" }
 
   attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id, :image, :image_front
 
