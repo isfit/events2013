9dc9a931da0216f521b46dbfc7ad5af124967b3a
Dag-Inge Aas <daginge@gmail.com>
2013-02-02 16:21:27 +0100

    trying another quality approach

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,24 @@ class Event < ActiveRecord::Base
   belongs_to :event_place
   has_many :event_dates
 
-  has_attached_file :image, :styles => { :cover => "920x420<", :square => "500x500>" }, :convert_options => { :all => "-quality 75" }
-  has_attached_file :image_front, :styles => { :cover => "920x220<", :square => "500x500>" }, :convert_options => { :all => "-quality 75" }
+  has_attached_file :image, 
+    :styles => { 
+      :cover => {
+        geometry: "920x420<",
+        quality: 80,
+        format: "JPG" }, 
+      :square => "500x500>" 
+    }
+  
+  has_attached_file :image_front,
+    :styles => { 
+      :cover =>  {
+        geometry: "920x220<",
+        quality: 70,
+        format: "JPG"
+      },
+      :square => "500x500>" 
+    }
 
   attr_accessible :body, :price_member, :price_other, :publish_at, :sidebar, :title, :event_type_id, :event_place_id, :image, :image_front
 
